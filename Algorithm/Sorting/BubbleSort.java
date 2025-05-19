f954a15c37f14931a55b7ce96e34051144130d05
NerfedChou <janrhian012904@gmail.com>
2025-05-19 12:03:53 +0800

    bitch this programs burns

--- Algorithm/Sorting/BubbleSort.java
+++ Algorithm/Sorting/BubbleSort.java
@@ -1,11 +1,12 @@
 package Algorithm.Sorting;
 
 public class BubbleSort {
+    // This function performs bubble sort
     public static void bubbleSort(int arr[]) {
         int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
+        for (int i = 0; i < n - 1; i++) { //Define maximum number of passes
+            for (int j = 0; j < n - i - 1; j++) { //Loop through the array
+                if (arr[j] > arr[j + 1]) {//Swap if the element found is greater than the next element
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
