d157356aef31f9baa54a2453fd714815e7cbcfc8
NerfedChou <janrhian012904@gmail.com>
2025-05-22 15:16:04 +0800

    Updated BubbleSort

--- Algorithm/Sorting/BubbleSort.java
+++ Algorithm/Sorting/BubbleSort.java
@@ -4,12 +4,13 @@ public class BubbleSort {
     // This function performs bubble sort
     public static void bubbleSort(int arr[]) {
         int n = arr.length;
-        for (int i = 0; i < n - 1; i++) { //Define maximum number of passes
-            for (int j = 0; j < n - i - 1; j++) { //Loop through the array
+        int max = n - 1;
+        for (int i = 0; i < max; i++) { //Define maximum number of passes
+            for (int j = 0; j < max - 1; j++) { //Loop through the array
                 if (arr[j] > arr[j + 1]) {//Swap if the element found is greater than the next element
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
+                    int temp = arr[j]; // Place the value
+                    arr[j] = arr[j + 1]; //Swap the value
+                    arr[j + 1] = temp; //Use the temps value
                 }
             }
         }
@@ -17,9 +18,8 @@ public class BubbleSort {
     // This function prints the array
 
     public static void printArray(int arr[]) {
-        int n = arr.length;
-        for (int i = 0; i < n; i++)
-            System.out.print(arr[i] + " ");
+        for (int i: arr)
+            System.out.print( i + " ");
         System.out.println();
     }
 
