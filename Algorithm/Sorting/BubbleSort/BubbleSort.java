acf6321f065689177064004ca8bb554849bdd9ce
NerfedChou <janrhian012904@gmail.com>
2025-05-23 21:33:37 +0800

    Added Visual Example on Bubble sort

--- Algorithm/Sorting/BubbleSort/BubbleSort.java
+++ Algorithm/Sorting/BubbleSort/BubbleSort.java
@@ -5,8 +5,8 @@ public class BubbleSort {
     public static void bubbleSort(int arr[]) {
         int n = arr.length;
         int max = n - 1;
-        for (int i = 0; i < max; i++) { //Define maximum number of passes
-            for (int j = 0; j < max - 1; j++) { //Loop through the array
+        for (int i = 0; i < max; i++) { 
+            for (int j = i; j < max ; j++) { //Loop through the array
                 if (arr[j] > arr[j + 1]) {//Swap if the element found is greater than the next element
                     int temp = arr[j]; // Place the value
                     arr[j] = arr[j + 1]; //Swap the value
